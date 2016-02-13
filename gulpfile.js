f632d904a49c5e95a62d55fdf36e23205335abcf
andy <andy@andydote.co.uk>
2016-02-13 17:47:56 +0000

    app: hookup jsx compilation

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,13 @@ var concat = require('gulp-concat');
 var react = require('gulp-react');
 var connect = require('gulp-connect');
 
-//gulp.task("default", [ "restore", "version", "compile", "test", "pack" ]);
-
-// gulp.task('transform', function() {
-//   return gulp
-//     .src(["./Magistrate/content/components/**/*.jsx"])
-//     .pipe(react())
-//     .pipe(concat("build.min.js"))
-//     .pipe(gulp.dest("./Magistrate/content"));
-// });
-//
-
-
+gulp.task('transform', function() {
+  return gulp
+    .src(['./src/**/*.jsx'])
+    .pipe(react())
+    .pipe(concat("build.min.js"))
+    .pipe(gulp.dest("./app"));
+});
 
 gulp.task('connect', function() {
   connect.server({
@@ -29,7 +24,8 @@ gulp.task('html', function () {
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['./app/*.htm'], ['html']);
+  gulp.watch(['./app/*.htm', './app/*.js'], ['html']);
+  gulp.watch(['./src/**/*.jsx'], ['transform']);
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['connect', 'watch', 'transform']);
---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+var App = React.createClass({
+
+  render() {
+    return (
+      <h1>Testing</h1>
+    );
+  }
+});
