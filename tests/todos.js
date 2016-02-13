5f3c09f0d717de0f89c9c5808495d312a42072fc
andy <andy@andydote.co.uk>
2016-02-13 20:21:34 +0000

    todos: use reducser combination

--- tests/todos.js
+++ tests/todos.js
@@ -40,6 +40,29 @@ const todos = (state = [], action) => {
   }
 };
 
+const visibilityFilter = (state = "SHOW_ALL", action) => {
+  switch (action.type) {
+    case "SET_VISIBILITY_FILTER":
+      return action.filter;
+
+    default:
+      return state;
+  }
+};
+
+const todoApp = (state = {}, action) => {
+  return {
+    todos: todos(state.todos, action),
+    visibilityFilter: visibilityFilter(state.visibilityFilter, action)
+  };
+};
+
+
+
+
+
+
+
 const testAddTodo = () => {
 
   const stateBefore = [];
