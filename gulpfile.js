67da31a75fc4c843d8cbbd5d1f72a7aca3b39f7d
andy <andy@andydote.co.uk>
2016-02-14 20:44:57 +0000

    gulp: use babel-react rather than gulp-react for react compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require("gulp");
 var concat = require('gulp-concat');
-var react = require('gulp-react');
 var connect = require('gulp-connect');
 var babel = require('gulp-babel');
 var shell = require('gulp-shell');
@@ -9,7 +8,7 @@ var debug = require('gulp-debug');
 gulp.task('transform', function() {
   return gulp
     .src(['./src/**/*.jsx'])
-    .pipe(react())
+    .pipe(babel({ presets: [ 'es2015', 'stage-2', 'react'] }))
     .pipe(concat("build.min.js"))
     .pipe(gulp.dest("./app"));
 });
@@ -32,10 +31,10 @@ gulp.task('watch', function () {
   gulp.watch(['./tests/**/*.js'], ['babel']);
 });
 
-gulp.task('babel', function() {
+gulp.task('test', function() {
   return gulp
     .src("./tests/**/*.js")
-    .pipe(babel({ presets: [ 'es2015', 'stage-2'] }))
+    .pipe(babel({ presets: [ 'es2015', 'stage-2', 'react'] }))
     .pipe(gulp.dest("./bin"))
     .pipe(shell(['node <%= file.path %>']));
 });
