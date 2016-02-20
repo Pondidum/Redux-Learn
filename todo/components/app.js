d1925d0b67e6c059e5352e5e47e6d2664373c856
andy <andy@andydote.co.uk>
2016-02-20 17:04:53 +0000

    app: implement visibility filter

--- todo/components/app.js
+++ todo/components/app.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import AddTodo from '../containers/addTodo'
 import VisibleTodoList from '../containers/visibleTodoList'
+import Footer from './footer'
 
 const App = () => (
   <div>
     <AddTodo />
     <VisibleTodoList />
+    <Footer />
   </div>
 )
 
--- todo/test/actions/todos.spec.js
+++ todo/test/actions/todos.spec.js
@@ -18,4 +18,11 @@ describe('todo actions', () => {
     })
   })
 
+  it('setVisibilityFilter should create SET_VISIBILITY_FILTER action', () => {
+    expect(actions.setVisibilityFilter('active')).toEqual({
+      type: 'SET_VISIBILITY_FILTER',
+      filter: 'active'
+    })
+  })
+
 })
