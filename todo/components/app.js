f80f4b9ed14a8592fec59afd51ff8291378e8f57
andy <andy@andydote.co.uk>
2016-02-20 16:15:19 +0000

    app: implement addTodo

--- todo/components/app.js
+++ todo/components/app.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import AddTodo from '../containers/addTodo'
 
 const App = () => (
   <div>
+    <AddTodo />
   </div>
 )
 
--- todo/reducers/index.js
+++ todo/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux'
+import todos from './todos'
 
-const todoApp = combineReducers({})
+const todoApp = combineReducers({
+  todos
+})
 
 export default todoApp
